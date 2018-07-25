b8559df896b17cf30f1b4e5c4cb7e53517efb231
Josh Reighley <josh@reighley.net>
2018-07-24 22:24:13 -0700

    Added move function

--- src/palara/palara.clj
+++ src/palara/palara.clj
@@ -13,27 +13,62 @@
 (defn luckymove? [] (= 20 (rolldie 20)))
 
 (defn actioncost [action]
-  (cond (= action :move) (if (luckymove?) 1 2)
+  (cond (= action :move)  2
    :else 1))
 
 (defn addplayer [username]
- (let [init-user { (keyword username) { :state {:inventory {:stone 0, :copper 0, :food 0, :wood 0,} :actions 0 :location :north}}}]
+ (let [init-user { (keyword username) { :state {:inventory {:stone 0, :copper 0, :food 0, :wood 0,} :actions 2 :location :north}}}]
    (swap! game conj init-user)))
 
+(defn reset-moves
+  "sets a player's actions to a specified quantity"
+  [player n]
+  (swap! game conj (-> @game
+                       (update-in [player :state] conj {:actions n}))))
 
-(defn harvest [player commodity]
+(defn reset-action-quota
+  "resets ever player's actions to 2"
+  []
+  (let [players (keys @game)]
+   (doall (map #(reset-moves % 2) players))
+   @game))
+
+(defn harvest
+   "updates games tate to give the specified player a random
+    dice roll of the comodity harvested"
+  [player commodity]
   (let [playerstate (:state (player @game))
         location (:location playerstate)
-        actioncount (:actions playerstate)
         harvestrate (->> harvestrates
                          location
                          commodity)
         harvestqty (->> (repeatedly #(rolldie 5))
                         (take harvestrate)
                         (reduce +))
-        newinv   (update-in @game [player :state :inventory commodity] + harvestqty)
-        newstate (update-in newinv [player :state :actions] - (actioncost :harvest))
-        actionstr (str "Harvested " harvestqty " " commodity ". You have " (->> newstate player :state :actions) " actions remaining for today. Your current stock is " (->> newstate player :state :inventory))]
-    (swap! game conj  newstate)
-    actionstr))
+        newstate (-> @game
+                     (update-in [player :state :inventory commodity] + harvestqty)
+                     (update-in [player :state :actions] - (actioncost :harvest)))
+        actionsleft  (->> newstate player :state :actions)
+        actionstr (str "Harvested " harvestqty " " commodity ". You have " actionsleft " actions remaining for today. Your current stock is " (->> newstate player :state :inventory))]
+    (if (neg? actionsleft)
+      (str "Not enough actions remaining to harvest " commodity)
+      (do (swap! game conj  newstate)
+          actionstr))))
 
+(defn move "updates game state to move player to specified destination region"
+  [player destination]
+  (let [playerstate (:state (player @game))
+        fromloc (:location playerstate)
+        moves (:actions playerstate)]
+   (cond (= fromloc destination) "Journey to nowhere"
+         (< moves 2) "No time to get there"
+    :else (swap! game conj (-> @game
+                               (update-in [player :state] conj {:location destination})
+                               (update-in [player :state] conj {:actions (- moves (actioncost :move))}))))))
+
+
+
+(defn init-test-game []
+  (def game (atom {}))
+  (addplayer "josh")
+  (addplayer :wilma))
